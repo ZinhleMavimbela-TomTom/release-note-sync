ae212a6d1d0fc45b31dfcb8cfd89ab00db70d113
ZinhleMavimbela-TomTom <99164103+ZinhleMavimbela-TomTom@users.noreply.github.com>
2022-03-22 16:15:43 +0800

    Update release_extractor.py

--- release_extractor.py
+++ release_extractor.py
@@ -86,8 +86,9 @@ def pulling_data():
                 sanitize = re.sub(r"[\n\t]*", "", d.text)
                 sanitize = re.sub(' +', " ", sanitize)
                 # replace the periods with periods and new line
-                if((sanitize).find('. ')):
-                    sanitize = re.sub('\.', ".\\n", sanitize)
+                sanitize = re.sub(r'(\d).(\d+)', r'\1-\2', sanitize)
+                sanitize = re.sub('\.', ".\n", sanitize)
+                sanitize = re.sub(r'(\d).(\d+)', r'\1.\2', sanitize)
                 sanitize = sanitize.strip()
                 descriptions.append(sanitize)
             #remove introduction and general from both country names and the descriptions
