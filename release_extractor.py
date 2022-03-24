f8529f3815229e7a2a56ff0de026c13598eebc9c
ZinhleMavimbela-TomTom <99164103+ZinhleMavimbela-TomTom@users.noreply.github.com>
2022-03-24 12:30:27 +0800

    Update release_extractor.py

--- release_extractor.py
+++ release_extractor.py
@@ -117,12 +117,12 @@ def print_all():
 
             
  
-#def pushing_data():
- #   for country in country_isocode_description:
- #       storage_db(country.data_ver,country.code, country.description)
+def pushing_data():
+    for country in country_isocode_description:
+	storage_db(country.data_ver,country.code, country.description)
 
 
 pulling_data()
 print("Total Country Count: ", len(country_isocode_description))
 print_all()
-#pushing_data()
+pushing_data()
